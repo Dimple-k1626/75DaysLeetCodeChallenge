0f26cee28cbb099544d9a477df4667797a152ed6
Dimple K <dimple.k1626official@gmail.com>
2026-08-28 21:08:23 +0530

    Time: 3 ms (59.21%), Space: 52.6 MB (26.5%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.java
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.java
@@ -1,16 +1,15 @@
-class Solution {
-    public int findMaxConsecutiveOnes(int[] nums) {
-        int maxi=0;
-        int cnt=0;
-        for(int num:nums){
-            if(num==1){
-                cnt++;
-                maxi=Math.max(maxi, cnt);
-            }
-            else{
-                cnt=0;
-            }
-        }
-        return maxi;
-    }
-}
+ class Solution {
+   public int findMaxConsecutiveOnes(int[] nums) {
+         int max = 0, count = 0;
+
+         for (int num : nums) {
+             if (num == 1) {
+                 count++;
+                 max = Math.max(max, count);
+             } else {
+                 count = 0;
+             }
+         }
+         return max;
+   }
+ }
