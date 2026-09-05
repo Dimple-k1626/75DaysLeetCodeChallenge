fb463ed1a75539f98a4d4052188202eceb1a33ab
Dimple K <dimple.k1626official@gmail.com>
2026-09-05 22:43:00 +0530

    Time: 0 ms (100%), Space: 55.8 MB (31.74%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,17 +1,23 @@
 class Solution {
-    public int majorityElement(int[] nums) {
-        HashMap<Integer, Integer> hash = new HashMap<>();
-        int res = 0;
-        int majority = 0;
-
-        for (int n : nums) {
-            hash.put(n, 1 + hash.getOrDefault(n, 0));
-            if (hash.get(n) > majority) {
-                res = n;
-                majority = hash.get(n);
-            }
-        }
-
-        return res;        
+    public static int majorityElement(int[] nums) {
+        return helper(nums,0,nums[0]);
+    }static int helper(int[] nums, int si, int ref){
+        int c=0;
+        for(int i=si;i<nums.length;i++){
+            if(nums[i]==ref)
+                c++;
+            else
+                c--;
+            if(c==-1)
+                return helper(nums,i,nums[i]);
+        }return ref;
+    }
+    public static void main(String[] args)throws Exception{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        String[] s=br.readLine().split(" ");
+        int[] nums=new int[s.length];
+        for(int i=0;i<s.length;i++){
+            nums[i]=Integer.parseInt(s[i]);
+        }majorityElement(nums);
     }
 }
